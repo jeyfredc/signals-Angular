fcdfcabbc489b77d0b2f97cb3aebed9cfefc76c5
jeyfred.calderon <jeyfred.calderon@artemisa.tech>
2023-08-22 07:43:50 -0500

    Finalizacion seccion 24 signals

--- src/app/signals/pages/counter-page/counter-page.component.ts
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   templateUrl: './counter-page.component.html',
   styleUrls: ['./counter-page.component.css']
 })
 export class CounterPageComponent {
+
+  public counter = signal(10)
+  /* Esto es una señal computada y la ventaja que tiene es que puede leer otras señales, esta se esta haciendo 
+  para hacer el counter al cuadrado y establecer el valor en el squareCounter */
+  public squareCounter = computed( ()=> this.counter() * this.counter()  )
+
+  increaseBy(value: number){
+
+    this.counter.update( current => current + value )
+  }
 
 }
--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './properties-page.component.html',
@@ -6,4 +7,55 @@ import { Component } from '@angular/core';
 })
 export class PropertiesPageComponent {
 
+  public user = signal<User>({
+    id:1,
+    email:'geoge.bluth@reqres.in',
+    first_name: 'George',
+    last_name: 'Bluth',
+    avatar:'https://reqres.in/img/faces/1-image.jpg'
+  })
+
+/* El agregar keyof hace que no se pueda recibir cualquier propiedad en el input si no que de forma obligada tienen
+que ser las propiedades que estan definidas en este caso del USer */
+  onFieldUpdated(field:keyof User, value:string){
+    
+    // this.user.set({
+    //   ...this.user(),
+    //   [field]:value
+    // })
+
+/*     Mutate es un dispirador de efectos, esto quiere decir que utilizando este ejemplo
+    cualquier cosa que se escriba sobre cualquier input va a disparar el efecto y en este caso la propiedad o value
+    firsr_name a penas se toque cualquier cosa de los input su valor cambiara enseguida a hola mundo
+    this.user.mutate(current => {
+      current.first_name = 'Hola mundo'
+    }) */
+
+
+    
+/*     this.user.mutate(current => {
+      switch (field) {
+        case 'email':
+        current.email = value;
+        break;
+        case 'first_name':
+        current.first_name = value;
+        break;
+        case 'last_name':
+        current.last_name = value;
+        break;
+      }
+    }) */
+
+    this.user.update( current =>{
+      return {
+        ...current,
+        [field]: value
+      }
+    })
+
+    /* Las tres anteriores son formas de hacer lo mismo, pero en el caso de que exista un numero se tendria que hacer la conversion a number
+    porque lo que se esta recibiendo en todos los campos son string */
+
+  }
 }
